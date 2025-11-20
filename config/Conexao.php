290db2f45d08e371e2a24123e860ec0b55deb064
izaac-eduardo <izaaceduardosoares24@gmail.com>
2025-11-19 22:32:41 -0300

    ajustada a identacao config.php

--- config/Conexao.php
+++ config/Conexao.php
@@ -1,18 +1,24 @@
 <?php
-    class Conexao {
-        private static $host = "localhost";
-        private static $usuario = "root";
-        private static $senha = "";
-        private static $banco = "geekstore";
 
-        public static function conectar() {
-            try {
+class Conexao
+{
+    private static $host = "localhost";
+    private static $usuario = "root";
+    private static $senha = "";
+    private static $banco = "geekstore";
 
-                return new PDO("mysql:host=".self::$host.";dbname=".self::$banco.";charset=utf8",
-                self::$usuario, self::$senha);
+    public static function conectar()
+    {
+        try {
 
-            } catch (PDOException $e) {
-                die("Erro ao conectar com banco de dadaos: {$e->getMessage()}");
-            }
+            return new PDO(
+                "mysql:host=" . self::$host . ";dbname=" . self::$banco . ";charset=utf8",
+                self::$usuario,
+                self::$senha
+            );
+
+        } catch (PDOException $e) {
+            die("Erro ao conectar com banco de dadaos: {$e->getMessage()}");
         }
     }
+}
